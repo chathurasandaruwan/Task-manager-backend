d8cffee92c7496bf9960c5ea9cc9398c978d0b0a
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-08 23:03:10 +0530

    create DataPersistException

---
+++ src/main/java/lk/ijse/Task_manager_backend/exeption/DataPersistException.java
@@ -0,0 +1,13 @@
+package lk.ijse.Task_manager_backend.exeption;
+
+public class DataPersistException extends RuntimeException{
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+
+    }
+    public DataPersistException(String message, Throwable cause) {
+
+    }
+}
