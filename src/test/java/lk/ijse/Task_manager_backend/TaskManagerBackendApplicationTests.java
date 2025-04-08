903e93833d33536220c9f456d9a2484564e0a7f3
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-08 22:37:37 +0530

    git init

---
+++ src/test/java/lk/ijse/Task_manager_backend/TaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.Task_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
