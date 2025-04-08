8632f35ad02ff4dd6a6590d75da85a258bbb4553
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-04-08 23:12:48 +0530

    create TaskNotFoundException

---
+++ src/main/java/lk/ijse/Task_manager_backend/exeption/TaskNotFoundException.java
@@ -0,0 +1,13 @@
+package lk.ijse.Task_manager_backend.exeption;
+
+public class TaskNotFoundException extends RuntimeException{
+    public TaskNotFoundException() {
+    }
+
+    public TaskNotFoundException(String message) {
+
+    }
+    public TaskNotFoundException(String message, Throwable cause) {
+
+    }
+}
